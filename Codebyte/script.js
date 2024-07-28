315f7d58072d4771739adfff10bc0575f444decd
shrikant-nomith <therealnomith@gmail.com>
2024-07-28 21:37:12 +0530

    Add files via upload

---
+++ Codebyte/script.js
@@ -0,0 +1,9 @@
+var crsr = document.querySelector("#cursor")
+var blur = document.querySelector("#cursor-blur")
+document.addEventListener("mousemove",function(dets){
+    crsr.style.left = dets.x+"px"
+    crsr.style.top = dets.y+"px"
+    blur.style.left = dets.x - 150 + "px"
+    blur.style.top = dets.y - 150 + "px"
+})
+
